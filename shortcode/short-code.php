b83730407e8b2e9cdd6cca8c05bc1d0b07b34125
EssDaniels <skibol2@wp.pl>
2023-02-16 14:53:53 +0100

    Update short-code.php
    Opóźnienie bufforowania

--- shortcode/short-code.php
+++ shortcode/short-code.php
@@ -3,7 +3,10 @@
 
 function ess_tabel_view_shortcode()
 {
-  ess_tabel_currenty_view();
+    ob_start();
+    ess_tabel_currenty_view();
+    $output = ob_get_clean();
+    return $output;
 }
 
 add_shortcode('ess_tabel_view_shortcode', 'ess_tabel_view_shortcode');
